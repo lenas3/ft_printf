404123cc6d18a478f1de2263179c2285faf349cf
lenas3 <sayaleyna27@gmail.com>
2025-07-09 19:18:54 +0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: asay <asay@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 18:37:38 by asay              #+#    #+#             */
-/*   Updated: 2025/07/07 18:36:43 by asay             ###   ########.fr       */
+/*   Updated: 2025/07/09 18:54:26 by asay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 int	ft_printf(const char *str, ...);
 int	ft_char(char c);
-int	ft_hex(long nb);
-int	ft_uphex(int nb);
+int	ft_hex(size_t nb);
+int	ft_uphex(long nb);
 int	ft_str(char *str);
 int	ft_ptr(void *ptr);
 int	ft_str(char *str);
