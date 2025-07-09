789029d944501e6befec7a37718e1ef5c5799a06
lenas3 <sayaleyna27@gmail.com>
2025-07-09 15:17:55 +0300

    Update explanation of ft_printf .c

--- explanation of ft_printf .c
+++ explanation of ft_printf .c
@@ -12,42 +12,19 @@
 
 #include "ft_printf.h"
 
-int	ft_putnbr(int nb)
-{
-	int i;
-	
-	i = 0;
-	if (nb == -2147483648)
-	{
-		ft_putchar('-');
-		write(1, "2147483648", 10);
-		return 0;
-	}
-	if (nb < 0)
-	{
-		i = 1;
-		ft_putchar('-');
-		nb = -nb;
-	}
-	if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		i++;
-	}
-	ft_putchar('0' + nb % 10);
-
-	return (i);
-}
-
-
 static int form(char x, va_list args)
 {
 	if (x == 's')
-		return (ft_str(va_arg(args, char *)));	
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			str = "(null)";
+		return (ft_str(str));
+	}
 	else if (x == 'd' || x == 'i')
-		return (ft_putnbr(va_arg(args, int)));
+		return (ft_nbr(va_arg(args, int)));
 	else if (x == 'c')
-		return (ft_putchar((char)va_arg(args, int)));
+		return (ft_char((char)va_arg(args, int)));
 /* 
 	Default Argument Promotion: variadic fonksiyonlar çağrıldığında küçük tamsaı tipleri büyük tam sayı tiplerine yükseltilit.
 		Sebep?
@@ -55,9 +32,8 @@ static int form(char x, va_list args)
 		argümanlar int olarak alınır. Eğer direkt int yerine char almış olsaydık va_arg'ın aldığı boyut ve tip,
 		argümanın bellek alanı ile uyuşmazdı. Yani gerçek argğman int olarak 4 byte kaplarken 1 byte gönderilmiş olacaktı.
 */
-		
 	else if (x == '%')
-		return (ft_putchar('%'));
+		return (ft_char('%'));
 	else if (x == 'p')
 		return (ft_ptr(va_arg(args, void *)));
 	else if (x == 'x')
@@ -67,53 +43,55 @@ static int form(char x, va_list args)
 	else if(x == 'X')
 		return (ft_uphex((long)va_arg(args, unsigned int)));
 	else if (x == 'u')
-		return ((unsigned int)ft_putnbr(va_arg(args, int)));
-	return (x);
-}
-int ft_isvalid(char a)
-{
-		if(a == 'c' || a == 's' ||a == 'p'||a == 'd'
-			||a == 'i'|| a == 'u' || a == 'x' || a == 'X' || a == '%'){
-			return (1);
-		}
-		return (0);
+		return (ft_unnbr(va_arg(args, unsigned int)));
+	return (0);
 }
 
-int ft_printf(const char *str, ...)
+static int	ft_hasvalid(const char *str, int j)
 {
-	int i;
-	int count;
-	va_list args;
-	va_start(args, str); // va_start cagrisi her zaman % kontrolunden once olmali.
-	// variable str is crucial otherwise i dunno how many args are.
+	int	count;
 
-	i = 0;
 	count = 0;
-	while(str[i])
+	while (str[j] != '\0')
 	{
-		if(str[i] == '%')
+		if (str[j] == 'c' || str[j] == 's' || str[j] == 'p' || str[j] == 'd'
+			|| str[j] == 'i' || str[j] == 'u' || str[j] == 'x'
+			|| str[j] == 'X' || str[j] == '%')
 		{
-				i++;
-				count += form(str[i], args);
-				i++;
-		}
-		if(str[i] == ' ')
-		{
-			if(ft_isvalid(str[i]))
-				i++;
+			return (2);
+			count++;
 		}
 		else
-			count += ft_putchar(str[i++]);
+			j++;
 	}
-	va_end(args);
 	return (count);
 }
 
-#include <stdio.h>
-
-int main()
+int	ft_printf(const char *str, ...)
 {
-	printf("original:%123");
-	printf("\n---\n");
-	ft_printf("asay:%123");
+	int		i;
+	int		count;
+	va_list	args;
+
+	va_start(args, str);
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			if (ft_hasvalid(str, i + 1) == 2)
+			{
+				i++;
+				count += form(str[i], args);
+				i++;
+			}
+			else
+				return (0);
+		}
+		else
+			count += ft_char(str[i++]);
+	}
+	va_end(args);
+	return (count);
 }
